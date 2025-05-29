e4479aaf31d6a608409e256bc4e4db4f6034c356
ArghyaInWeb <ab.arghyabiswas2030@gmail.com>
2025-05-29 13:36:46 +0530

    Learned: Variables & Datatypes

---
+++ 01_basics/01-1_variables.js
@@ -0,0 +1,13 @@
+const accountId = 1824; //? const variable - can not change value after assigned
+let accountEmail = "arghya@gmail.com"; 
+var accountPassword = "2090"
+accountCity = "Kolkata"; //! NOT RECOMMENDED
+
+
+//accountId = 23; //! NOT ALLOWED
+accountEmail = "ab@gmail.com";
+accountPassword = "2498";
+accountCity = "Mumbai"; //! NOT RECOMMENDED
+
+console.log(accountId); 
+console.table([accountId, accountEmail, accountPassword, accountCity]); //? Displays data in a tabular format
---
+++ 01_basics/01-2_datatypes.js
@@ -0,0 +1,35 @@
+"use strict"; //? Any code below will now work in modern Javasript.
+
+//* Datatypes
+//* Primitive Datatypes
+
+//* NUMBER 
+let a = 40; //? Interger Number
+let b = 3.14; //? Float Number
+console.log(a, b);
+
+//* STRING 
+let name = "Arghya";
+let char = "@"
+console.log(name, char);
+
+//* BOOLEAN 
+let isOnline = true;
+let isOffline = false;
+console.log(isOnline, isOffline);
+
+//* UNDEFINED
+let notDefined;
+console.log(notDefined);
+
+//* NULL
+let empty = null;
+console.log(empty);
+
+//* BIGINT
+let big = 123456789012345678901234567890n;
+console.log(big);
+
+//* SYMBOL
+let sym = Symbol("id");
+console.log(sym);
