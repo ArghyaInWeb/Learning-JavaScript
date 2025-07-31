92ce036e1ea7bc80b87702d61e63ac6e37d5f36e
ArghyaInWeb <ab.arghyabiswas2030@gmail.com>
2025-07-31 19:20:22 +0530

    Project: To-Do List; UI is done

---
+++ 13_More_Projects/To-Do List/script.js
@@ -0,0 +1,7 @@
+const todoInput = document.querySelector('.todo-input')
+const addTodoBtn = document.querySelector('.add-button')
+const todoList = document.querySelector('.todo-list')
+
+const todoItem = document.querySelectorAll('todoItem')
+const checkBox = document.querySelector('.checkbox')
+const todoText = document.querySelector('.todo-text')
